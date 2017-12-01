e51b4bd70ff8c4c1dea96b46d3d0379eeaa81fe5
Adam Juhasz <adam.juhasz@crowdflower.com>
2017-11-30 19:30:41 -0800

    more images

--- cell.js
+++ cell.js
@@ -2,29 +2,39 @@ const fs = require('fs');
 const _ = require('lodash');
 const makeid = require('./idgen');
 
-const cell1 = fs.readdirSync(`${__dirname}/public/images/cell1`).filter(e => /.png$/.test(e)).map(e => ({
-  match: e.match(/(\d+)/g),
-  name: `/images/cell1/${e}`,
-  id: `${makeid(10)}-${makeid(10)}-${makeid(10)}`,
-}));
+function readCellDir(dir) {
+  const cell1 = fs.readdirSync(`${__dirname}/public/images/${dir}`).filter(e => /.png$/.test(e)).map(e => ({
+    match: e.match(/(\d+)/g).reverse(),
+    name: `images/${dir}/${e}`,
+    id: `${makeid(10)}-${makeid(10)}-${makeid(10)}`,
+  }));
 
-const cell1Job = {};
-cell1.forEach((e) => {
-  const address = e.match.join('.');
-  cell1Job[address] = e;
-});
+  const cellJob = {};
+  cell1.forEach((e) => {
+    const address = e.match.join('.');
+    cellJob[address] = e;
+  });
+  return cellJob;
+}
+
+const jobs = {};
+jobs.cell1 = readCellDir('cell1');
+jobs.cell2 = readCellDir('cell2');
+
 // console.log(cell1Job);
 
 function getWork(qs) {
   const root = qs.root || '';
+  const taskToken = qs.tasktoken || 'cell1';
   const images = [];
   for (let i = 1; i <= 16; i += 1) {
     const address = _.compact([].concat(root, i)).join('.');
-    images.push(cell1Job[address]);
+    console.log(jobs[taskToken][address]);
+    images.push(jobs[taskToken][address]);
   }
   return {
     task: 'where cells touch each other',
-    taskToken: root,
+    taskToken,
     images: images.map(image => ({ id: image.id, src: image.name, selected: false })),
   };
 }
